fceabcf209e50e1064419b856b007347dfe5b9b8
you jigang <97567344@qq.com>
2018-10-23 10:39:57 +0800

    Update NumberSequence/NumberSequence/Program.cs

---
+++ NumberSequence/NumberSequence/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberSequence
+{
+    internal class Program
+    {
+        //f(1) = 1, f(2) = 1, f(n) = (A * f(n - 1) + B * f(n - 2)) mod 7.
+        private static int NumberSequence(int a, int b, int n)
+        {
+            if (n > 2)
+                return (a * NumberSequence(a, b, n - 1) + b * NumberSequence(a, b, n - 2)) % 7;
+            else
+                return 1;
+        }
+
+        private static void Main(string[] args)
+        {
+            int a = 1, b = 2, n = 10;
+            Console.WriteLine(NumberSequence(a, b, n).ToString());
+            Console.Read();
+        }
+    }
+}
